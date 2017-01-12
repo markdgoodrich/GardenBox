1f540e1a78b3631abb42851388388bef2278de2c
markdgoodrich <mark.d.goodrich@gmail.com>
2017-01-12 10:02:02 -0600

    Add files via upload

--- LemonTreePi.py
+++ LemonTreePi.py
@@ -7,9 +7,6 @@ import time
 from envirophat import light
 from envirophat import weather
 
-##def plant_diagnostic():
-##    print "Water level is %s. The plant has recieved %d hours of sunlight" %(water(), solar())
-
 
 def solar():
     
@@ -18,13 +15,13 @@ def solar():
     l = light.light()
 
     if time.localtime()[4] == 15:
-        sun = "%d.%d   %d" %(time.localtime()[3], time.localtime()[4]+10, l)       #This format allows me to import the txt file to the Grapher application
-    elif time.localtime()[4] == 30:
-        sun = "%d.%d   %d" %(time.localtime()[3], time.localtime()[4]+20, l)        #These adjsutments coorespond time to a corrext x-axis position
+        sun = "%d.%d   %d" %(time.localtime()[3], time.localtime()[4]+10, l)        #This format allows me to import the txt file ...
+    elif time.localtime()[4] == 30:                                                 #...to the Mac Grapher application
+        sun = "%d.%d   %d" %(time.localtime()[3], time.localtime()[4]+20, l)        #These adjustments coorespond time to a corrext x-axis position
     elif time.localtime()[4] == 45:
         sun = "%d.%d   %d" %(time.localtime()[3], time.localtime()[4]+30, l)
     else:
-        sun = "%d.%d   %d" %(time.localtime()[3], time.localtime()[4], l)           #when minutes = 0 , new hour      
+        sun = "%d.%d   %d" %(time.localtime()[3], time.localtime()[4], l)           #when minutes = 0, no need to convert to coordiante      
         
     sun_data.write(sun)
     sun_data.write("\n")
@@ -33,9 +30,9 @@ def solar():
     return sun
 
 
-def temperature():
+def temperature():  #Need to calibrate this; it's reading CPU heat
     
-    temp_data = open("tempdata_%d_%d_%d.txt" %(year, month, day), "ab") #year, month, day variables
+    temp_data = open("tempdata_%d_%d_%d.txt" %(year, month, day), "ab") 
 
     fahrenheit = 1.8*float(weather.temperature())+32
 
@@ -51,7 +48,7 @@ def temperature():
 
 
 
-while True:                                                                 #runs constantly
+while True:                                                                     #runs constantly
     second = time.localtime()[5]
     minute = time.localtime()[4]         
     hour = time.localtime()[3]
@@ -60,35 +57,32 @@ while True:                                                                 #run
     year = time.localtime()[0]
     
 
-    if hour == 0:                                                           #at the start of a new day...
-        sun_data = open("sundata_%d_%d_%d.txt" %(year, month, day), "ab")       #... create new files with the date
-#        temp_data = open("tempdata_%d_%d_%d.txt" %(year, month, day), "ab")            #temperature data
+#    if hour == 0: #This is probably not necessaryy                              #at the start of a new day...
+#        sun_data = open("sundata_%d_%d_%d.txt" %(year, month, day), "ab")       #... create new files with the date
+#        temp_data = open("tempdata_%d_%d_%d.txt" %(year, month, day), "ab")           #temperature data
 
 
     
-    if minute % 15 == 0 and second == 0:                 #every 15 minutes
+    if minute % 15 == 0 and second == 0:                #Records every 15 minutes
         solar()
-        time.sleep(1)
+        time.sleep(1)                                   #Pause to prevent duplicate data
         
-                                           #to prevent duplicate data
-
-#----Below is a rough execution of the temperature test every 2 hours------#
-
-    if hour % 2 == 0 and minute == 0 and second == 3:           #The idea is that, every two hours, on the first second, this will excecute once
+    if hour % 2 == 0 and minute == 0 and second == 3:   #Records every 2 hours, after solar data is collected
         temperature()
-        time.sleep(2)
+        time.sleep(1)                                   #Pause to prevent duplicate data
         
 
 
 
-#This latest update now creates a new file to store the solar data in at the beginning of each day.
-#Also, the light data is now in a form that can be imported by Grapher.  This includes proper adjsut of time to x-axis coordinates
+#This update:
+#   Cleaned up a good bit of the code, and made the comments easier to understand
+#   Hid the beggining-of-the-day loop (if hour == 0:), since the files should be created and opened properly when their functions are called anyways.
+#       I will delete this once I'm sure it's not necessary
+#   No duplicate data issues, which were caused by multiple 'nohup' executes in ther Terminal. (My bad)
 
 
 
-
-#Things to do:  Calibrate temperature sensor. Use A/C readout, as well as candy termometer status
+#Things to do:  Calibrate temperature sensor. Use A/C readout, as well as candy termometer.
+#                   Rewrite temperature function to accomodate
 #               Write Soil moisture sensor function
 #               Have the pi email me every day with the old data (sun, temperature, soil status)
-
-#STOP   using multiple 'nohup' commands
