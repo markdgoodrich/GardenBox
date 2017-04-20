560cd00b7218d6fe99b38087ed014e2394906318
Mark Goodrich <mark.d.goodrich@gmail.com>
2017-04-20 07:30:00 -0500

    Update Record_Data.py

--- Record_Data.py
+++ Record_Data.py
@@ -10,6 +10,6 @@ print "    Data Recording Setup\n"
 
 username = raw_input("Enter your username (ex. owner1): ")
 ip = raw_input("Enter the IP address (ex. 192.168.0.XXX): ")
-location = raw_input("Enter exported data location (ex. ~/Documents): ")
+location = raw_input("Enter exported data location (ex. /Documents): ")
 
 os.system("nohup python GardenBox.py %s %s %s &" %(username, ip, location))
